25f3969eeb22ebb9de41bccd71d00b2de9955029
Ben Dechrai <ben@dechrai.com>
2020-07-13 12:26:46 +1000

    Reactivate Auth0 in dev branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,63 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import { useAuth0 } from "@auth0/auth0-react"
-import { Link } from "gatsby"
+import { useAuth0 } from "@auth0/auth0-react"
+import { Link, graphql } from "gatsby"
+import imageUrlBuilder from '@sanity/image-url'
 
-const CTA = () => {
-  // const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0()
+export const query = graphql`
+  {
+    programs: allSanityProgram(filter:{ highlighted:{eq:true}}) {
+      edges {
+        node {
+          name
+          slug {
+            current
+          }
+          description
+          image {
+            asset {
+              _id
+            }
+            crop {
+              top
+              bottom
+              left
+              right
+            }
+            hotspot {
+              x
+              y
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-  // if (isLoading || isAuthenticated) return null
+const IndexPage = ({ data }) => {
 
-  return (
-    <div className="cta">
-      {/* <button onClick={loginWithRedirect} className="button">Login <i className="icon-signin"></i></button> */}
-      {/* <span>- or -</span> */}
-      {/* <button onClick={loginWithRedirect} className="button">Sign Up Today <i className="icon-edit2"></i></button> */}
-      <h3>This platform is still under development.</h3>
-      <p>Want to join the development process? <Link to="/build" className="button">Build Hoist with us <i className="icon-laptop"></i></Link></p>
-      <p>Want to join the community and help define this platform? <a href="https://community.hoist.earth/" className="button" target="hoist-community">Join the Community <i className="icon-comments"></i></a></p>
-    </div>
-  )
-}
+  const builder = imageUrlBuilder({
+    projectId: process.env.GATSBY_SANITY_PROJECTID,
+    dataset: process.env.GATSBY_SANITY_DATASET,
+  })
 
-const IndexPage = () => {
+  const CTA = () => {
+    const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0()
+
+    if (isLoading || isAuthenticated) return null
+
+    return (
+      <div className="cta">
+        <button onClick={loginWithRedirect} className="button">Login <i className="icon-signin"></i></button>
+        <span>- or -</span>
+        <button onClick={loginWithRedirect} className="button">Sign Up Today <i className="icon-edit2"></i></button>
+      </div>
+    )
+  }
 
   return (
     <Layout>
@@ -31,6 +67,9 @@ const IndexPage = () => {
         <h2>Fund effective climate change programs with a single payment</h2>
         <p>Whether you’re offsetting your annual family carbon footprint or your work-related travel impact, Hoist takes the hard work out of donating effectively.</p>
         <CTA />
+        <h3>This platform is still under development.</h3>
+        <p className="join-us-content"><span>Want to join the development process?</span> <Link to="/build" className="button">Build Hoist with us <i className="icon-laptop"></i></Link></p>
+        <p className="join-us-content"><span>Want to join the community and help define this platform?</span> <a href="https://community.hoist.earth/" className="button" target="hoist-community">Join the Community <i className="icon-comments"></i></a></p>
       </section>
 
       <section id="features">
@@ -57,6 +96,20 @@ const IndexPage = () => {
             <p>However involved you are, you’ll always know what’s happening. From clear reports on where we’re investing money, to the latest news on the current climate fixing strategies, you’ll always know how you’re helping.</p>
           </div>
         </div>
+      </section>
+
+      <section id="programs">
+        {data.programs.edges.map(program => (
+          <div className="program">
+            <h3><Link to={"programs/" + program.node.slug.current}>
+              {program.node.name}
+            </Link></h3>
+            <p><Link to={"programs/" + program.node.slug.current}>
+              <img src={builder.image(program.node.image).width(300).height(150).url()} alt={program.node.name} />
+            </Link></p>
+            <p>{program.node.description}</p>
+          </div>
+        ))}
       </section>
 
     </Layout>
