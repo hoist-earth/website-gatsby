7c6130c73ad8ec909fe28dd5b5987488d3effa5e
Ben Dechrai <ben@dechrai.com>
2020-07-14 15:00:02 +1000

    Redirect back to origin after logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,39 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import './header.css'
+import "./header.css"
 import { useAuth0 } from "@auth0/auth0-react"
 
 const UserNav = () => {
-
   const { isLoading, isAuthenticated, logout, loginWithRedirect } = useAuth0()
 
   if (isLoading) return null
 
-  if (isAuthenticated) return (
-    <ul>
-      <li><button className="button" onClick={logout}>Logout</button></li>
-    </ul>
-  )
+  if (isAuthenticated)
+    return (
+      <ul>
+        <li>
+          <button
+            className="button"
+            onClick={() => {
+              logout({ returnTo: window.location.origin })
+            }}
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    )
 
   return (
     <ul>
-      <li><button className="button" onClick={loginWithRedirect}>Login</button></li>
+      <li>
+        <button className="button" onClick={loginWithRedirect}>
+          Login
+        </button>
+      </li>
     </ul>
   )
-
 }
 
 const partlyActive = className => ({ isPartiallyCurrent }) => ({
@@ -32,14 +44,40 @@ const Header = () => {
   return (
     <header>
       <div>
-        <Link to="/"><img src="/images/logo.png" alt="Hoist Logo" /></Link>
+        <Link to="/">
+          <img src="/images/logo.png" alt="Hoist Logo" />
+        </Link>
       </div>
       <nav>
         <ul>
-          <li><Link to="/" activeClassName="active">Home</Link></li>
-          <li><Link to="/about" getProps={partlyActive()} activeClassName="active">About</Link></li>
-          <li><Link to="/build" getProps={partlyActive()} activeClassName="active">Build</Link></li>
-          <li><Link to="/account" activeClassName="active">Account</Link></li>
+          <li>
+            <Link to="/" activeClassName="active">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              getProps={partlyActive()}
+              activeClassName="active"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/build"
+              getProps={partlyActive()}
+              activeClassName="active"
+            >
+              Build
+            </Link>
+          </li>
+          <li>
+            <Link to="/account" activeClassName="active">
+              Account
+            </Link>
+          </li>
         </ul>
         <UserNav />
       </nav>
