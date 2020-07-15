66308151c00a76d3aeef49b25bd84e64b1631acd
Ben Dechrai <ben@dechrai.com>
2020-07-15 10:19:18 +1000

    Reformatting changes, plus fixed up JSON array for image metadata

--- src/pages/build/2020-07-02-creating-the-hoist-api.js
+++ src/pages/build/2020-07-02-creating-the-hoist-api.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Layout from '../../components/layout'
-import SEO from '../../components/seo'
-import { useStaticQuery } from 'gatsby'
+import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import { useStaticQuery } from "gatsby"
 
 const Development = () => {
   const { site } = useStaticQuery(
@@ -21,36 +21,46 @@ const Development = () => {
         <SEO
           title="Creating the Hoist API | Build"
           description="In this session, I'll be creating a new Express API from scratch, and creating new content types in Sanity in preparation for the joint stream on Monday with Thor Schaeff from Stripe."
-          meta={{
-            name: `twitter:image`,
-            content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-02.png`,
-          }, {
-            name: `og:image`,
-            content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-02.png`,
-            prefix: `og: http://ogp.me/ns#`,
-          }} />
-        <img className="fullwidth" src="/images/build/streams/2020-07-02.png" alt="Live Coding: Creating the Hoist API" />
+          meta={[
+            {
+              name: `twitter:image`,
+              content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-02.png`,
+            },
+            {
+              name: `og:image`,
+              content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-02.png`,
+              prefix: `og: http://ogp.me/ns#`,
+            },
+          ]}
+        />
+        <img
+          className="fullwidth"
+          src="/images/build/streams/2020-07-02.png"
+          alt="Live Coding: Creating the Hoist API"
+        />
         <section>
-          <p><a href="https://twitch.tv/bendechrai">
-            <span role="img" aria-label="calendar">📆</span>{" "}
-            Streaming live on Twitch: Thursday 2nd July at 1pm AEST
-          </a></p>
+          <p>
+            <a href="https://twitch.tv/bendechrai">
+              <span role="img" aria-label="calendar">
+                📆
+              </span>{" "}
+              Streaming live on Twitch: Thursday 2nd July at 1pm AEST
+            </a>
+          </p>
           <h2>2nd July 2020 - Creating the Hoist API</h2>
           <p>
-            In this session, I'll be creating a new Express API from scratch, and creating new content
-            types in Sanity in preparation for the joint stream on Monday with Thor Schaeff from Stripe.
+            In this session, I'll be creating a new Express API from scratch,
+            and creating new content types in Sanity in preparation for the
+            joint stream on Monday with Thor Schaeff from Stripe.
           </p>
-          <p>
-            By the end of the stream, we should have:
-          </p>
+          <p>By the end of the stream, we should have:</p>
           <ul>
             <li>Set up a new Express API;</li>
             <li>Deployed the API to Vercel;</li>
-            <li>Created an endpoint to update users in Auth0 via Management API:
+            <li>
+              Created an endpoint to update users in Auth0 via Management API:
               <ul>
-                <li>
-                  we'll be storing the Stripe Customer ID in there later;
-                </li>
+                <li>we'll be storing the Stripe Customer ID in there later;</li>
               </ul>
             </li>
             <li>Created a Subscription Plan content type in Sanity; and</li>
--- src/pages/build/2020-07-06-adding-payments-with-stripe.js
+++ src/pages/build/2020-07-06-adding-payments-with-stripe.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Layout from '../../components/layout'
-import SEO from '../../components/seo'
-import { useStaticQuery } from 'gatsby'
+import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import { useStaticQuery } from "gatsby"
 
 const Development = () => {
   const { site } = useStaticQuery(
@@ -21,34 +21,49 @@ const Development = () => {
         <SEO
           title="Adding Payments with Stripe | Build"
           description="In this session, I'll be joined by Thor Schaeff from Stripe in order to add subscription payments to Hoist."
-          meta={{
-            name: `twitter:image`,
-            content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-06.png`,
-          }, {
-            name: `og:image`,
-            content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-06.png`,
-            prefix: `og: http://ogp.me/ns#`,
-          }} />
-        <img className="fullwidth" src="/images/build/streams/2020-07-06.png" alt="Live Coding: Adding Payments with Stripe" />
+          meta={[
+            {
+              name: `twitter:image`,
+              content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-06.png`,
+            },
+            {
+              name: `og:image`,
+              content: `${site.siteMetadata.siteUrl}/images/build/streams/2020-07-06.png`,
+              prefix: `og: http://ogp.me/ns#`,
+            },
+          ]}
+        />
+        <img
+          className="fullwidth"
+          src="/images/build/streams/2020-07-06.png"
+          alt="Live Coding: Adding Payments with Stripe"
+        />
         <section>
-          <p><a href="https://twitch.tv/bendechrai">
-            <span role="img" aria-label="calendar">📆</span>{" "}
-            Streaming live on Twitch: Monday 6th July at 1pm AEST
-          </a></p>
+          <p>
+            <a href="https://twitch.tv/bendechrai">
+              <span role="img" aria-label="calendar">
+                📆
+              </span>{" "}
+              Streaming live on Twitch: Monday 6th July at 1pm AEST
+            </a>
+          </p>
           <h2>6th July 2020 - Adding Payments with Stripe</h2>
           <p>
-            In this session, I'll be joined by <a href="https://twitter.com/thorwebdev">Thor Schaeff</a>
-            {" "}
-            from <a href="https://twitter.com/stripe">Stripe</a> in order to add subscription payments
-            to Hoist.
+            In this session, I'll be joined by{" "}
+            <a href="https://twitter.com/thorwebdev">Thor Schaeff</a> from{" "}
+            <a href="https://twitter.com/stripe">Stripe</a> in order to add
+            subscription payments to Hoist.
           </p>
-          <p>
-            Join us to learn how to:
-            </p>
+          <p>Join us to learn how to:</p>
           <ul>
-            <li>add Stripe Checkout and the new customer portal to your Gatsby app;</li>
+            <li>
+              add Stripe Checkout and the new customer portal to your Gatsby
+              app;
+            </li>
             <li>link your Auth0 users with Stripe customers;</li>
-            <li>pull current subscription information from the Stripe API; and</li>
+            <li>
+              pull current subscription information from the Stripe API; and
+            </li>
             <li>programatically cancel and manage your users subscriptions.</li>
           </ul>
         </section>
