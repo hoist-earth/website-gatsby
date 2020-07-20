93d8be8db6b87171ce8411157029444d8fd810ab
Ben Dechrai <ben@dechrai.com>
2020-07-21 00:42:17 +1000

    Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import useFeatureFlags from "../hooks/use-feature-flags"
 
 export const query = graphql`
   {
-    programs: allSanityProgram(filter:{ highlighted:{eq:true}}) {
+    programs: allSanityProgram(filter: { highlighted: { eq: true } }) {
       edges {
         node {
           name
@@ -56,9 +56,13 @@ const IndexPage = ({ data }) => {
 
     return (
       <div className="cta">
-        <button onClick={loginWithRedirect} className="button">Login <i className="icon-signin"></i></button>
+        <button onClick={loginWithRedirect} className="button">
+          Login <i className="icon-signin"></i>
+        </button>
         <span>- or -</span>
-        <button onClick={loginWithRedirect} className="button">Sign Up Today <i className="icon-edit2"></i></button>
+        <button onClick={loginWithRedirect} className="button">
+          Sign Up Today <i className="icon-edit2"></i>
+        </button>
       </div>
     )
   }
@@ -66,38 +70,72 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-
       <section id="intro">
         <h2>Fund effective climate change programs with a single payment</h2>
-        <p>Whether you’re offsetting your annual family carbon footprint or your work-related travel impact, Hoist takes the hard work out of donating effectively.</p>
+        <p>
+          Whether you’re offsetting your annual family carbon footprint or your
+          work-related travel impact, Hoist takes the hard work out of donating
+          effectively.
+        </p>
         <CTA />
         <h3>This platform is still under development.</h3>
-        <p className="join-us-content"><span>Want to join the development process?</span> <Link to="/build" className="button">Build Hoist with us <i className="icon-laptop"></i></Link></p>
-        <p className="join-us-content"><span>Want to join the community and help define this platform?</span> <a href="https://community.hoist.earth/" className="button" target="hoist-community">Join the Community <i className="icon-comments"></i></a></p>
+        <p className="join-us-content">
+          <span>Want to join the development process?</span>{" "}
+          <Link to="/build" className="button">
+            Build Hoist with us <i className="icon-laptop"></i>
+          </Link>
+        </p>
+        <p className="join-us-content">
+          <span>Want to join the community and help define this platform?</span>{" "}
+          <a
+            href="https://community.hoist.earth/"
+            className="button"
+            target="hoist-community"
+          >
+            Join the Community <i className="icon-comments"></i>
+          </a>
+        </p>
       </section>
-
       <section id="features">
         <h2>Hoist lets you engage however you like</h2>
-        <p>From set-and-forget to an active community, Hoist is designed to work for everyone.</p>
+        <p>
+          From set-and-forget to an active community, Hoist is designed to work
+          for everyone.
+        </p>
         <div className="feature">
           <div className="feature-box">
             <i className="icon icon-check1" />
             <h3>Set and Forget</h3>
-            <p>We willl always invest in the best programs we can find. This option gives you the peace of mind that you’re contributing as effectively as we are. If you like, you can also join the community forum for more involvement, but there’s certainly no requirement for this.</p>
+            <p>
+              We willl always invest in the best programs we can find. This
+              option gives you the peace of mind that you’re contributing as
+              effectively as we are. If you like, you can also join the
+              community forum for more involvement, but there’s certainly no
+              requirement for this.
+            </p>
           </div>
         </div>
         <div className="feature">
           <div className="feature-box">
             <i className="icon icon-comments-alt" />
             <h3>Community Driven</h3>
-            <p>We will always listen to our community. If you have an idea of where to invest, we’re all ears. The old adage of many hands making light work is certainly the differantiator here.</p>
+            <p>
+              We will always listen to our community. If you have an idea of
+              where to invest, we’re all ears. The old adage of many hands
+              making light work is certainly the differantiator here.
+            </p>
           </div>
         </div>
         <div className="feature">
           <div className="feature-box">
             <i className="icon icon-dashboard" />
             <h3>Always Informed</h3>
-            <p>However involved you are, you’ll always know what’s happening. From clear reports on where we’re investing money, to the latest news on the current climate fixing strategies, you’ll always know how you’re helping.</p>
+            <p>
+              However involved you are, you’ll always know what’s happening.
+              From clear reports on where we’re investing money, to the latest
+              news on the current climate fixing strategies, you’ll always know
+              how you’re helping.
+            </p>
           </div>
         </div>
       </section>
@@ -105,18 +143,28 @@ const IndexPage = ({ data }) => {
       <section id="programs">
         {featurePrograms &&
           data.programs.edges.map(program => (
-          <div className="program">
-            <h3><Link to={"programs/" + program.node.slug.current}>
-              {program.node.name}
-            </Link></h3>
-            <p><Link to={"programs/" + program.node.slug.current}>
-              <img src={builder.image(program.node.image).width(300).height(150).url()} alt={program.node.name} />
-            </Link></p>
-            <p>{program.node.description}</p>
-          </div>
-        ))}
+            <div className="program">
+              <h3>
+                <Link to={"programs/" + program.node.slug.current}>
+                  {program.node.name}
+                </Link>
+              </h3>
+              <p>
+                <Link to={"programs/" + program.node.slug.current}>
+                  <img
+                    src={builder
+                      .image(program.node.image)
+                      .width(300)
+                      .height(150)
+                      .url()}
+                    alt={program.node.name}
+                  />
+                </Link>
+              </p>
+              <p>{program.node.description}</p>
+            </div>
+          ))}
       </section>
-
     </Layout>
   )
 }
