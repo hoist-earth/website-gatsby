5399f3ef53b21ce66a27653b2afa8b138e9938dc
Ben Dechrai <ben@dechrai.com>
2020-07-21 13:24:30 +1000

    Styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,22 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+const NotFoundPage = ({ location }) => (
+  <>
+    <Layout fourohfour={true}>
+      <SEO title="ʕ•ᴥ•ʔ" location={location} />
+      <section id="intro"></section>
+    </Layout>
+    <div className="fourohfour-overlay">
+      <h1>ʕ•ᴥ•ʔ</h1>
+      <p>I'm sorry. I can't find that page.</p>
+      <p>
+        <Link to="/">Back to home page</Link>
+      </p>
+    </div>
+  </>
 )
 
 export default NotFoundPage
