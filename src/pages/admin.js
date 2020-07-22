c7cb3ccfa6106070d8162f662d324234a97a5417
Ben Dechrai <ben@dechrai.com>
2020-07-23 01:51:46 +1000

    Added test button

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,17 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
-import { withAuthenticationRequired } from "@auth0/auth0-react"
+import { withAuthenticationRequired, useAuth0 } from "@auth0/auth0-react"
 import SEO from "../components/seo"
 import AuthRedirectMessage from "../components/auth-redirect-message"
 import CheckRole from "../components/check-role"
 
 const Admin = ({ location }) => {
+  const { getAccessTokenSilently } = useAuth0()
+
+  const testAdmin = () => {
+    getAccessTokenSilently().then(token => {
+      fetch(process.env.GATSBY_HOIST_API + "/admin/test", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then(res => res.json())
+        .then(res => {
+          alert(JSON.stringify(res))
+        })
+    })
+  }
+
   return (
     <Layout>
       <CheckRole roles={["admin"]}>
         <SEO title="Admin" location={location} />
         <section>
           <h1>Admin Dashboard</h1>
+          <button onClick={testAdmin}>Test Admin</button>
         </section>
       </CheckRole>
     </Layout>
